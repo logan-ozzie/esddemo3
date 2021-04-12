e484dbfc072881740160dfe7b289970b1b70a18b
logan-ozzie <61157926+logan-ozzie@users.noreply.github.com>
2021-04-12 09:38:40 +0100

    Add files via upload

---
+++ myfileESD3.py
@@ -0,0 +1,12 @@
+try:
+	n = int(input('Enter a number : '))
+	reversed = 0
+	
+	while(n!=0):
+		r=int(n%10)
+		reversed = reversed*10 + r
+		n=int(n/10)
+		
+	print(reversed)
+except ValueError:
+	print('Given input is not a number.')
